2c98b01212cc87369d63a60795baed783a13a26b
david <david.vidal@tecnativa.com>
2022-10-07 00:05:18 +0200

    Bump version for new release to 0.5.0

--- tests/test_odooshow.py
+++ tests/test_odooshow.py
@@ -3,4 +3,4 @@ from odooshow import __version__
 
 def test_version():
     # TODO: Add tests. Not easy though as we need to setup a minimum Odoo instance.
-    assert __version__ == "0.4.0"
+    assert __version__ == "0.5.0"
